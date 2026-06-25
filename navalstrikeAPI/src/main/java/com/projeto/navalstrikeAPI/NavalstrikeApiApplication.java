2ce66e2d9950098d6cafc80f747082c19e3958a6
GuilhermeFiedler <lhermetfied@gmail.com>
2026-06-25 10:13:31 -0300

    chore: commit inicial do projeto de batalha naval - backend.

---
+++ navalstrikeAPI/src/main/java/com/projeto/navalstrikeAPI/NavalstrikeApiApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.navalstrikeAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavalstrikeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavalstrikeApiApplication.class, args);
+	}
+
+}
---
+++ navalstrikeAPI/src/test/java/com/projeto/navalstrikeAPI/NavalstrikeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.navalstrikeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavalstrikeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
