e636bc55049ec66d8d334c417ad41601f67d8219
GuilhermeFiedler <lhermetfied@gmail.com>
2026-07-10 09:32:54 -0300

    chore: deploy render

--- navalstrikeAPI/src/main/java/com/projeto/navalstrikeAPI/NavalstrikeApiApplication.java
+++ navalstrikeAPI/src/main/java/com/projeto/navalstrikeAPI/NavalstrikeApiApplication.java
@@ -1,5 +1,6 @@
 package com.projeto.navalstrikeAPI;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -17,6 +18,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NavalstrikeApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(NavalstrikeApiApplication.class, args);
 	}
 
