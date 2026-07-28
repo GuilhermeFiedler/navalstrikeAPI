6f7693f95a5d75508f8d75d4ca6db25eedaa0ec4
GuilhermeFiedler <lhermetfied@gmail.com>
2026-07-28 12:01:05 -0300

    feat: observability config

---
+++ navalstrikeAPI/src/main/java/com/projeto/navalstrikeAPI/infra/tracing/ObservabilityConfig.java
@@ -0,0 +1,33 @@
+package com.projeto.navalstrikeAPI.infra.tracing;
+
+import io.micrometer.observation.ObservationPredicate;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.observation.ServerRequestObservationContext;
+import org.springframework.security.config.observation.SecurityObservationSettings;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
+
+@Configuration
+public class ObservabilityConfig {
+
+    @Bean
+    public ObservationPredicate noActuatorObservations() {
+        PathMatcher pathMatcher = new AntPathMatcher();
+        return (name, context) -> {
+            if (context instanceof ServerRequestObservationContext serverContext) {
+                String path = serverContext.getCarrier().getRequestURI();
+                return !pathMatcher.match("/actuator/**", path);
+            }
+            return true;
+        };
+    }
+
+    @Bean
+    public SecurityObservationSettings securityObservationSettings() {
+        return SecurityObservationSettings.withDefaults()
+                .shouldObserveAuthorizations(false)
+                .shouldObserveAuthentications(false)
+                .build();
+    }
+}
