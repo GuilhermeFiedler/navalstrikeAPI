ecdb016ddb22214b8f1e7db7d4e65f0e6b1715fc
GuilhermeFiedler <lhermetfied@gmail.com>
2026-07-29 08:47:10 -0300

    fix: traces spam

--- navalstrikeAPI/src/main/java/com/projeto/navalstrikeAPI/infra/security/SecurityConfig.java
+++ navalstrikeAPI/src/main/java/com/projeto/navalstrikeAPI/infra/security/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -98,16 +97,8 @@ public class SecurityConfig {
                 .build();
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/actuator/health", "/actuator/health/**");
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
     }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-}
-
-
-
